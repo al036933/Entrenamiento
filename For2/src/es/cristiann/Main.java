0eafaed5f69a5ce141315689209feece1bff53a6
Cristian García Hernández <al036933@ieslaencanta.com>
2022-01-18 12:53:14 +0100

    Añado proyecto For2.

---
+++ For2/src/es/cristiann/Main.java
@@ -0,0 +1,9 @@
+package es.cristiann;
+
+ class Main {
+     public static void main(String[] args) {
+         for (int i = 0; i <= 10; i += 2) {
+             System.out.println(i);
+         }
+    }
+}
